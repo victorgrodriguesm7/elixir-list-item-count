0af7c965aa6ceb0aa08f0484114ee555432fa5e1
Victor Rodrigues <victor.rodrigues@hibrido.com.br>
2021-12-06 08:59:27 -0300

    Implementation

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    sum(list, 0)
+  end
+
+  defp sum([], acc) do
+    acc
+  end
+
+  defp sum([ _head | tail ], acc) do
+    acc = acc + 1
+    sum(tail, acc)
   end
 end
